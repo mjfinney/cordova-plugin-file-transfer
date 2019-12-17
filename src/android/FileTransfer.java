554026a507223b53309b92eb2b5cad514ad72e2b
mjfinney <mjfinney@gmail.com>
2019-12-16 21:35:10 -0600

    Add import for java.net.URL

--- src/android/FileTransfer.java
+++ src/android/FileTransfer.java
@@ -32,6 +32,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Iterator;
